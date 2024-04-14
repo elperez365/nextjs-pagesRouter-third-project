cac95d18b1b91b1188c9ddcfe91fc52fa4b629b2
vasco ceti <vasco.ceti@gmail.com>
2024-04-14 12:36:54 +0200

    head component, meta tag and _document file

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          />
+        </Head>
+        <body>
+          <div id="overlays"></div>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
